e7b07d6cac7914c2675770c3b8bae95c71489f2d
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-07-03 21:07:53 +0000

    chore: upgrade dependencies

--- __tests__/split-hash-validator.spec.ts
+++ __tests__/split-hash-validator.spec.ts
@@ -1,6 +1,7 @@
 import { SplitHashValidator, NotMatchedError } from '@src/split-hash-validator'
 import { KiB, getSampleBuffer, getSampleStream, createHexHash } from '@test/utils'
 import { hashList128KiB, hashList150KiB } from '@test/fixtures/hash-list'
+import { go } from '@blackglory/go'
 
 describe('SplitHashValidator', () => {
   // 8 same size parts
@@ -14,30 +15,32 @@ describe('SplitHashValidator', () => {
         const validator = new SplitHashValidator(hashList, blockSize, createHexHash)
         stream
           .pipe(validator)
-          .on('error', done.fail)
-          .on('end', done)
+          .on('error', e => done(e))
+          .on('end', () => done())
           .resume()
       })
 
-      it('return same data', async done => {
-        const buffer = await getSampleBuffer()
-        const stream = getSampleStream()
-        const hashList = hashList128KiB
-        const blockSize = 128 * KiB
+      it('return same data', done => {
+        go(async () => {
+          const buffer = await getSampleBuffer()
+          const stream = getSampleStream()
+          const hashList = hashList128KiB
+          const blockSize = 128 * KiB
 
-        const validator = new SplitHashValidator(hashList, blockSize, createHexHash)
-        const output: number[] = []
-        stream
-          .pipe(validator)
-          .on('data', chunk => output.push(...chunk))
-          .on('error', done.fail)
-          .on('end', () => {
-            if (buffer.every((x, i) => output[i] === x)) {
-              done()
-            } else {
-              done.fail()
-            }
-          })
+          const validator = new SplitHashValidator(hashList, blockSize, createHexHash)
+          const output: number[] = []
+          stream
+            .pipe(validator)
+            .on('data', chunk => output.push(...chunk))
+            .on('error', e => done(e))
+            .on('end', () => {
+              if (buffer.every((x, i) => output[i] === x)) {
+                done()
+              } else {
+                done('error')
+              }
+            })
+        })
       })
     })
 
@@ -106,30 +109,32 @@ describe('SplitHashValidator', () => {
         const validator = new SplitHashValidator(hashList, blockSize, createHexHash)
         stream
           .pipe(validator)
-          .on('error', done.fail)
-          .on('end', done)
+          .on('error', e => done(e))
+          .on('end', () => done())
           .resume()
       })
 
-      it('return same data', async done => {
-        const buffer = await getSampleBuffer()
-        const stream = getSampleStream()
-        const hashList = hashList150KiB
-        const blockSize = 150 * KiB
+      it('return same data', done => {
+        go(async () => {
+          const buffer = await getSampleBuffer()
+          const stream = getSampleStream()
+          const hashList = hashList150KiB
+          const blockSize = 150 * KiB
 
-        const validator = new SplitHashValidator(hashList, blockSize, createHexHash)
-        const output: number[] = []
-        stream
-          .pipe(validator)
-          .on('data', chunk => output.push(...chunk))
-          .on('error', done.fail)
-          .on('end', () => {
-            if (buffer.every((x, i) => output[i] === x)) {
-              done()
-            } else {
-              done.fail()
-            }
-          })
+          const validator = new SplitHashValidator(hashList, blockSize, createHexHash)
+          const output: number[] = []
+          stream
+            .pipe(validator)
+            .on('data', chunk => output.push(...chunk))
+            .on('error', e => done(e))
+            .on('end', () => {
+              if (buffer.every((x, i) => output[i] === x)) {
+                done()
+              } else {
+                done('error')
+              }
+            })
+        })
       })
     })
 
