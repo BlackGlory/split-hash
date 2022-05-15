569e4b8b1a88d84161a1e6fb991eda1d12214449
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-05-15 09:08:14 +0000

    refactor: use private modifiers

--- src/split-hash-validator.ts
+++ src/split-hash-validator.ts
@@ -2,55 +2,45 @@ import { ProgressiveHashFactory, ProgressiveHash } from '@src/types'
 import { Transform, TransformCallback } from 'stream'
 
 export class SplitHashValidator<T> extends Transform {
-  #digests: T[]
-  #blockSize: number
-  #createHash: ProgressiveHashFactory<T>
-  #hash: ProgressiveHash<T>
-  #equals: (a: T, b: T) => boolean
-  #accu = 0
-  #digestIndex = 0
+  private hash: ProgressiveHash<T> = this.createHash()
+  private accu = 0
+  private digestIndex = 0
 
   constructor(
-    digests: T[]
-  , blockSize: number
-  , createHash: ProgressiveHashFactory<T>
-  , equals: (a: T, b: T) => boolean = Object.is
+    private digests: T[]
+  , private blockSize: number
+  , private createHash: ProgressiveHashFactory<T>
+  , private equals: (a: T, b: T) => boolean = Object.is
   ) {
     super()
-
-    this.#digests = digests
-    this.#blockSize = blockSize
-    this.#createHash = createHash
-    this.#hash = this.#createHash()
-    this.#equals = equals
   }
 
   _transform(chunk: Buffer, encoding: BufferEncoding, callback: TransformCallback): void {
     // chunk is always Buffer, encoding is always 'buffer', so there is no need to check
 
-    if (this.#accu + chunk.length < this.#blockSize) {
-      this.#hash.update(chunk)
-      this.#accu += chunk.length
+    if (this.accu + chunk.length < this.blockSize) {
+      this.hash.update(chunk)
+      this.accu += chunk.length
     } else {
       let offset = 0
       while (true) {
-        const needed = this.#blockSize - this.#accu
+        const needed = this.blockSize - this.accu
         const slice = chunk.slice(offset, offset + needed)
         if (slice.length === needed) {
-          this.#hash.update(slice)
-          const digest = this.#hash.digest()
-          if (!this.#equals(this.#digests[this.#digestIndex], digest)) {
+          this.hash.update(slice)
+          const digest = this.hash.digest()
+          if (!this.equals(this.digests[this.digestIndex], digest)) {
             return callback(new NotMatchedError())
           }
-          this.#digestIndex++
+          this.digestIndex++
           // prepare for the next round
-          this.#hash = this.#createHash()
-          this.#accu = 0
+          this.hash = this.createHash()
+          this.accu = 0
           offset += slice.length
         } else {
           // if the length does not match, the remaining data is not long enough, update the remaining data and exit the loop.
-          this.#hash.update(slice)
-          this.#accu += slice.length
+          this.hash.update(slice)
+          this.accu += slice.length
           break
         }
       }
@@ -60,15 +50,15 @@ export class SplitHashValidator<T> extends Transform {
   }
 
   _flush(callback: TransformCallback): void {
-    if (this.#accu > 0) {
-      const digest = this.#hash.digest()
-      if (!this.#equals(this.#digests[this.#digestIndex], digest)) {
+    if (this.accu > 0) {
+      const digest = this.hash.digest()
+      if (!this.equals(this.digests[this.digestIndex], digest)) {
         return callback(new NotMatchedError())
       }
-      this.#digestIndex++
+      this.digestIndex++
     }
 
-    if (this.#digestIndex !== this.#digests.length) {
+    if (this.digestIndex !== this.digests.length) {
       return callback(new NotMatchedError())
     }
 
